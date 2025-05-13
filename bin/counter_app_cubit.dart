897d0fd6ae618e9988f16af1c45cb18c1ad78bc6
Sidi Chrif Ahmed Maadh <155539380+sidi-maadh@users.noreply.github.com>
2025-05-13 01:18:29 +0000

    Update counter_app_cubit.dart

--- bin/counter_app_cubit.dart
+++ bin/counter_app_cubit.dart
@@ -8,15 +8,35 @@ class CounterCubit extends Cubit {
   void decrement() => emit(state - 1);
 }
 
+class CounterEvent {}
+
+class IncrementCounterEvent extends CounterEvent {}
+
+class DecrementCounterEvent extends CounterEvent {}
+
+class CounterBloc extends Bloc<CounterEvent, int> {
+  CounterBloc() : super(0) {
+    on<IncrementCounterEvent>((event, emit) => emit(state + 1));
+    on<DecrementCounterEvent>((event, emit) => emit(state - 1));
+  }
+}
+
 void main(List<String> arguments) {
+  
   final cubit = CounterCubit();
+  final bloc = CounterBloc();
 
   cubit.stream.listen(print);
+  bloc.stream.listen(print);
 
   cubit.increment();
   cubit.increment();
+  bloc.add(IncrementCounterEvent());
+  bloc.add(IncrementCounterEvent());
 
   cubit.decrement();
+  bloc.add(DecrementCounterEvent());
 
   cubit.close();
+  bloc.close();
 }
